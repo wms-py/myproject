66e620adf517ff10f00f27c7791a5cfa72945d03
wumingsheng <wu_mingsheng@126.com>
2020-03-27 16:21:25 +0800

    init

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date    : 2020-03-26 23:31:35
+# @Author  : wms (wu_mingsheng@126.com)
+
+
+from flask import Flask
+
+
+def (parameter_list):
+  pass
+
